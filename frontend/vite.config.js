8cdb4dc58d5c1f168f2e3573639b9c77f64782d9
MANISH KUMAR PANKAJ <manishkumarpankaj119@gmail.com>
2026-01-06 02:53:11 +0530

    Configure GitHub Pages Deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
+    base: "/model/", // REQUIRED for GitHub Pages
     server: {
         host: true,
         proxy: {
